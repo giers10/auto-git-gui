2abd1773904eb53d77c28b0cd00e27ea5371052a
Victor Giers <mail@victorgiers.com>
2025-05-23 23:49:01 +0200

    auto-git:  [change] renderer.js

--- renderer.js
+++ renderer.js
@@ -91,14 +91,6 @@ window.addEventListener('DOMContentLoaded', async () => {
           </svg>
           <span class="truncate text-sm font-medium">${basename(folder)}</span>
         </div>
-        <button class="remove-btn">
-          <svg xmlns="http://www.w3.org/2000/svg"
-               class="h-5 w-5"
-               fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"/>
-          </svg>
-        </button>
       `;
 
         // play/pause Button korrekt initialisieren
