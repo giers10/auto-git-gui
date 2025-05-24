372aae08dbffe7656fc1bbb749f7fc9857876ab9
Victor Giers <mail@victorgiers.com>
2025-05-24 14:52:47 +0200

    auto-git:  [change] main.js

--- main.js
+++ main.js
@@ -229,6 +229,7 @@ Here are the combined diffs:
 
 Even if this might seem like a lot of code, I need you to answer VERY BRIEFLY. A git commit message to be precise is what I need from you, to protocol these changes.
 Don't give any feedback on the code! Just analyze what changed and write the git commit message. Keep it short! A commit message MUST NOT BE STRAIGHT TO THE POINT!
+Don't reply to my message, just give me the commit message.`;
   } else {
     throw new Error('No commits found for LLM prompt.');
   }
