32bf0fb1a800c5ce40075a97ca02fa23925c2643
Victor Giers <mail@victorgiers.com>
2025-05-26 00:47:11 +0200

    Add test comment to handle LLM runs

--- main.js
+++ main.js
@@ -667,7 +667,7 @@ async function autoCommit(folderPath, message) {
       
       //const cands = folders[idx].llmCandidates;
       await runLLMCommitRewrite(folders[idx]);
-      //folders[idx].linesChanged = 0; // !!!!!!!!!!!!!!!!!!!!  needs logic to handle several llm runs called at the same time 
+      //folders[idx].linesChanged = 0; // !!!!!!!!!!!!!!!!!!!!  needs logic to handle several llm runs called at the same time test
       //folders[idx].llmCandidates = [];
       //folders.[idx].firstCandidateBirthday = null
     store.set('folders', folders);
