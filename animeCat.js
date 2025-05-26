583ce878df37d5515000ed3f794a77ef71a130ea
Victor Giers <mail@victorgiers.com>
2025-05-26 04:11:50 +0200

    Further increase move threshold for mouse hold

--- animeCat.js
+++ animeCat.js
@@ -140,7 +140,7 @@ _bindMouseHold() {
   let holdTimer = null;
 
   const CAT_TOLERANCE = 36;
-  const MOVE_THRESHOLD = 100;
+  const MOVE_THRESHOLD = 1450;
 
   const isMouseNearCat = (e) => {
     const rect = this.img.getBoundingClientRect();
