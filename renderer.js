da5ed7f89fdf6b897b26e39228659e5fb56a9b58
Victor Giers <mail@victorgiers.com>
2025-05-26 10:52:01 +0200

    Adjust commitsToday color calculation in renderer.js

--- renderer.js
+++ renderer.js
@@ -371,7 +371,7 @@ async function updateInteractionBar(folderObj) {
   }
 
   // In die Bar schreiben
-  const [r, g, b] = getCommitColor(10);
+  const [r, g, b] = getCommitColor(0);
   document.getElementById('commitsToday').textContent = commitsToday;
   document.getElementById('commitsToday').style.color = `rgb(${r},${g},${b})`;
   document.getElementById('linesUntilRewrite').textContent = linesUntilRewrite;
