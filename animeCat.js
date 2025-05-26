e5e5acc012b7321598ae4408e5de7825e4159589
Victor Giers <mail@victorgiers.com>
2025-05-26 11:23:15 +0200

    Reduce chance of salto in joy animation to 20%

--- animeCat.js
+++ animeCat.js
@@ -416,7 +416,7 @@ _runJoyAnimation(onFinish) {
   img.src = this.images.joy || this.images.default;
 
   // 20% Chance auf Salto!
-  const salto = Math.random() < 1;
+  const salto = Math.random() < 0.2;
 
   // Bubble abkoppeln
   this.container.appendChild(this.bubble);
