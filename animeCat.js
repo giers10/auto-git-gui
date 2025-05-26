ab18908bff47614ad54eabff0a02ac41f5b7d916
Victor Giers <mail@victorgiers.com>
2025-05-26 07:45:16 +0200

    Reduce width of glow element

--- animeCat.js
+++ animeCat.js
@@ -61,7 +61,6 @@ window.AnimeCat = class AnimeCat {
     this.glow.id = 'cat-glow';
     Object.assign(this.glow.style, {
       position:        'absolute',
-      transform:       'translate(-50%, -50%)',
       borderRadius:    '50%',
       border: '1px solid red',
       width:           '62px',     // vorher 120px
