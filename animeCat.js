74db19885b9abcb830107db752bfea198e7e24a0
Victor Giers <mail@victorgiers.com>
2025-05-26 08:02:15 +0200

    Adjust top position of glow element

--- animeCat.js
+++ animeCat.js
@@ -63,7 +63,7 @@ window.AnimeCat = class AnimeCat {
     Object.assign(this.glow.style, {
       position:        'absolute',
       left:            '40%',
-      top:             '74%',
+      top:             '73%',
       transform:       'translate(-50%, -50%)',
       borderRadius:    '50%',
       width:           '70px',      // ODER: Teste 54, 70, 80 – wie es dir am besten gefällt!
