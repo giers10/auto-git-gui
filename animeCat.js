0b06a3acacee7d00e1c1b6c9b730632cbcd9490f
Victor Giers <mail@victorgiers.com>
2025-05-26 07:58:19 +0200

    auto-git:  [change] animeCat.js

--- animeCat.js
+++ animeCat.js
@@ -66,7 +66,7 @@ window.AnimeCat = class AnimeCat {
       top:             '74%',
       transform:       'translate(-50%, -50%)',
       borderRadius:    '50%',
-      width:           '74px',      // ODER: Teste 54, 70, 80 – wie es dir am besten gefällt!
+      width:           '70px',      // ODER: Teste 54, 70, 80 – wie es dir am besten gefällt!
       height:          '50px',
       pointerEvents:   'none',
       zIndex:          1,
