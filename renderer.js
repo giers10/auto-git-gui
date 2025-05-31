f69977de11b8c7e62e153bd4a5b60771c305b92d
Victor Giers <mail@victorgiers.com>
2025-05-31 19:11:39 +0200

    auto-git:  [change] renderer.js

--- renderer.js
+++ renderer.js
@@ -543,7 +543,7 @@ async function startLiveCountdown(folderObj, msLeft) {
     
     // Dynamischer Renderbutton
     // statt dessen lieber den tree auslesen?
-    const readmePath = path.join(folder, 'README.md');
+    const readmePath = folder.join(folder, 'README.md');
     if (fs.existsSync(readmePath)) {
       readmeBtn.textContent = 'Update README';
     } else {
