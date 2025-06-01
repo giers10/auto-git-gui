fe0d2a95264aea16f36a314b22c73aef91d07785
Victor Giers <mail@victorgiers.com>
2025-06-01 09:42:48 +0200

    auto-git:  [change] main.js

--- main.js
+++ main.js
@@ -26,39 +26,13 @@ folders = folders.map(f => ({
 store.set('folders', folders);
 console.log("Startup-Folders:", store.get('folders'));
 
+
+
 let tray = null;
 let isQuiting = false;
 
 
 
-function createTray(win) {
-  const iconPath = path.join(__dirname, 'assets/icon/trayicon.png');
-  const icon = nativeImage.createFromPath(iconPath);
-
-  // Standard-Größen je nach OS
-  let size;
-  switch (process.platform) {
-    case 'darwin': // macOS
-      size = { width: 22, height: 22 };
-      break;
-    case 'win32':  // Windows
-      size = { width: 16, height: 16 };
-      break;
-    default:       // Linux / other
-      size = { width: 24, height: 24 };
-  }
-
-  const trayImage = icon.resize(size);
-  const tray = new Tray(trayImage);
-
-  tray.setToolTip('Auto-Git läuft im Hintergrund');
-  tray.on('double-click', () => {
-    win.show();
-    win.focus();
-  });
-
-  return tray;
-}
 
 if (Array.isArray(folders)) {
   folders = folders.map(f => ({
